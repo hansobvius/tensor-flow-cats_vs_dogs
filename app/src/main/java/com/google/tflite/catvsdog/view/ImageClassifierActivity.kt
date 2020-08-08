8b147065626530887e37b354a3ce577442deb59e
Thiago Caiado <thiago.caiado@hotmail.com>
2020-08-08 18:40:37 -0300

    migrating to androidx

--- app/src/main/java/com/google/tflite/catvsdog/view/ImageClassifierActivity.kt
+++ app/src/main/java/com/google/tflite/catvsdog/view/ImageClassifierActivity.kt
@@ -1,12 +1,12 @@
 package com.google.tflite.catvsdog.view
 
-import android.databinding.DataBindingUtil
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.google.tflite.catvsdog.R
 import com.google.tflite.catvsdog.databinding.ActivityImageClassifierBinding
 import com.google.tflite.catvsdog.tflite.Classifier
