fddc469ed8fec6235a2647bc3a690222a0588daf
Thiago Caiado <thiago.caiado@hotmail.com>
2020-08-13 20:58:30 -0300

    create image classifier layout

--- app/src/main/java/com/google/tflite/catvsdog/view/fragment/ImageClassifierFragment.kt
+++ app/src/main/java/com/google/tflite/catvsdog/view/fragment/ImageClassifierFragment.kt
@@ -5,10 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.tflite.catvsdog.databinding.FragmentImageClassifierBinding
+import kotlinx.android.synthetic.main.fragment_image_classifier.view.*
 
 class ImageClassifierFragment: Fragment() {
 
+    private lateinit var binding: FragmentImageClassifierBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentImageClassifierBinding.inflate(inflater).apply{
+            this.lifecycleOwner = this@ImageClassifierFragment
+        }
+        return binding.root
     }
 }
